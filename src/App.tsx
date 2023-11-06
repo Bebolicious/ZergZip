e4fe8c0fafbeeac8760dcf87ff6fa3fdf1217815
Alex <alex@lvira.dev>
2023-11-07 00:18:30 +0100

    ZZ-1: Fix some errors, implement file selection

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,14 @@ import { DropZoneContainer } from "./lib/fileDrop/dropzone";
 import CloseIcon from "@mui/icons-material/Close";
 
 function App() {
-  const [files, setFiles] = useState<string[] | undefined>();
-  async function zipFile() {
-    await invoke("compress", {
-      source: "FlightPlan.pdf",
-      target: "compressed_file" + DEFAULT_FILE_EXTENSION,
-    });
-  }
+  const [files, setFiles] = useState<string[] | undefined>(undefined);
+  const [fileSelection, setFileSelection] = useState<string[] | undefined>();
+  // async function zipFile() {
+  //   await invoke("compress", {
+  //     source: "FlightPlan.pdf",
+  //     target: "compressed_file" + DEFAULT_FILE_EXTENSION,
+  //   });
+  // }
 
   const fileUpload = async (_files: string[]) => {
     await addFiles(_files);
@@ -37,6 +38,20 @@ function App() {
     setFiles(undefined);
   }
 
+  function handleFiles(_files: string[]) {
+    if (_files.length >= 1) {
+      setFileSelection(_files);
+    } else {
+      setFileSelection(undefined);
+    }
+  }
+
+  async function removeFiles() {
+    setFiles(
+      await invoke("set_files", { method: "remove", files: fileSelection })
+    );
+  }
+
   return (
     <FileDropContext fn={fileUpload}>
       <Box
@@ -49,18 +64,39 @@ function App() {
           backgroundColor: "transparent",
         }}
       >
-        <Typography
-          sx={{
-            fontSize: "50px",
-            fontFamily: "Gothic Regular",
-            marginBottom: "20px",
-            color: "white",
-          }}
-        >
-          {files ? "Add or remove files" : "Welcome to ZergZip!"}
-        </Typography>
+        {files ? (
+          <Typography
+            sx={{
+              fontSize: "50px",
+              fontFamily: "Gothic Regular",
+              marginBottom: "20px",
+              color: "white",
+              userSelect: "none",
+            }}
+          >
+            Add or remove files
+          </Typography>
+        ) : (
+          <Typography
+            sx={{
+              fontSize: "50px",
+              fontFamily: "Gothic Regular",
+              marginBottom: "20px",
+              color: "white",
+              userSelect: "none",
+              display: "flex",
+            }}
+          >
+            Welcome to
+            <Box sx={{ marginLeft: "12px", color: "#7d5c9c" }}>ZergZip</Box>!
+          </Typography>
+        )}
 
-        {files ? <FileCards files={files} /> : <DropZoneContainer />}
+        {files ? (
+          <FileCards handleFiles={handleFiles} files={files} />
+        ) : (
+          <DropZoneContainer />
+        )}
 
         <Box sx={{ margin: "20px 00px 0px 0px" }}>
           {!files ? (
@@ -78,7 +114,6 @@ function App() {
                 },
               }}
               variant="outlined"
-              onClick={() => {}}
               endIcon={
                 <FolderZipIcon
                   sx={{ padding: "0px 0px 2px 10px" }}
@@ -93,6 +128,38 @@ function App() {
                 }}
               >
                 Select files
+              </Typography>
+            </Button>
+          ) : fileSelection ? (
+            <Button
+              sx={{
+                padding: "0.75rem 1.3rem",
+                backgroundColor: "#2a2e32",
+                color: "white",
+                boder: "1px solid black",
+                borderColor: "#2a2e32",
+
+                "&:hover": {
+                  backgroundColor: "#3c4145",
+                  borderColor: "#3c4145",
+                },
+              }}
+              variant="outlined"
+              onClick={() => removeFiles()}
+              endIcon={
+                <CloseIcon
+                  sx={{ padding: "0px 0px 2px 10px" }}
+                  fontSize="large"
+                />
+              }
+            >
+              <Typography
+                sx={{
+                  fontFamily: "Gothic Regular",
+                  textTransform: "none",
+                }}
+              >
+                Remove Selection
               </Typography>
             </Button>
           ) : (
